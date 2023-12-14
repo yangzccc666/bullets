00b0b96d1f4bee2ac29382e2e5a3a1d70236d276
yangzccc666 <yangzccc@gmail.com>
2023-12-14 19:21:51 +0800

    rewrite js/dist/game.js

--- bullets_game/static/js/src/zbase.js
+++ bullets_game/static/js/src/zbase.js
@@ -1,17 +1,4 @@
-export class AcGame {
-        constructor(id, AcWingOS) {
-                    this.id = id;
-                    this.$ac_game = $('#' + id);
-                    this.AcWingOS = AcWingOS;
-
-                    this.settings = new Settings(this);
-                    this.menu = new AcGameMenu(this);
-                    this.playground = new AcGamePlayground(this);
-
-                    this.start();
-                }
-
-        start() {
-                }
+class AcGame {
+        constructor(id) {
+        }
 }
-
